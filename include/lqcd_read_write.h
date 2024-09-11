891c675ad8c254e4283294b57d536b694dd01860
YellowBicycleee <wangjianchengcs@163.com>
2024-09-11 23:59:26 +0800

    modify: 对io做出部分修改

--- include/lqcd_read_write.h
+++ include/lqcd_read_write.h
@@ -6,12 +6,26 @@
 #include "lattice_desc.h"
 #include <string>
 struct LatticeIOHandler {
+    static constexpr uint32_t QCU_READ_MODE  = O_RDONLY;
+    static constexpr uint32_t QCU_WRITE_MODE = O_WRONLY | O_CREAT;
+    static constexpr uint32_t QCU_READ_WRITE_MODE = O_RDWR | O_CREAT;
+    // static constexpr uint32_t QCU_WRITE_CREATE_MODE = O_WRONLY | O_CREAT;
+
     bool file_opened = false;
     int fd = -1;
-    const char *file_path = nullptr;
+    const char *file_path_ = nullptr;
 
-    LatticeIOHandler(const char *file_path) : file_path(file_path) {
-        fd = open(file_path, O_RDWR); // O_RDWR: Read and write
+    LatticeIOHandler(const std::string& file_path, const int32_t file_open_mode) 
+                            : file_path_(file_path.c_str()) 
+    {
+        if (file_open_mode != QCU_READ_MODE && file_open_mode != QCU_WRITE_MODE && 
+            file_open_mode != QCU_READ_WRITE_MODE) {
+            throw std::runtime_error("Invalid file open mode");
+        }
+
+        mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH; // rw-rw-rw-
+        // fd = open(file_path_, O_RDWR | O_CREAT, mode); // O_RDWR: Read and write
+        fd = open(file_path_, file_open_mode, mode); // O_RDWR: Read and write
         if (fd == -1) {
             throw std::runtime_error(std::string("Failed to open file ") + file_path);
         }
@@ -31,7 +45,8 @@ void read_from_file (
         std::complex<_FloatType> *device_src_data,       // 数据指针
         std::complex<_FloatType> *device_dst_data,       // 数据指针
         std::complex<_FloatType> *device_gauge_data,     // 数据指针
-        const char *file_path                       // 文件路径
+        const std::string& file_path,                    // 文件路径
+        LatticeConfig& lattice_config
     );
 
 template <typename _FloatType = double>
@@ -39,6 +54,6 @@ void write_to_file (
         std::complex<_FloatType> *device_src_data,       // 数据指针
         std::complex<_FloatType> *device_dst_data,       // 数据指针
         std::complex<_FloatType> *device_gauge_data,     // 数据指针
-        const char *file_path,                        // 文件路径
+        const std::string& file_path,                          // 文件路径
         LatticeConfig& lattice_config
     );
--- src/lqcd_read_write.cu
+++ src/lqcd_read_write.cu
@@ -11,119 +11,87 @@
 using std::complex;
 using std::cout;
 using std::endl;
-// #include <fcntl.h>
-// 默认双精度读写
+
+
+
+
+template <typename _FloatType>
+static void write_to_file_memcpy_GPU (
+        void* file_ptr,                           // 文件映射指针
+        std::complex<_FloatType> *device_src_data,       // 数据指针
+        std::complex<_FloatType> *device_dst_data,       // 数据指针
+        std::complex<_FloatType> *device_gauge_data,      // 数据指针
+        const LatticeConfig& lattice_config
+    )
+{
+    auto mrhs_colorspinor_length = lattice_config.MrhsColorSpinorLength();
+    auto gauge_length = lattice_config.GaugeLength();
+
+    if (lattice_config.m_io_position & IOPosition::IO_FERMION_IN) {
+        CHECK_CUDA(cudaMemcpy(file_ptr, device_src_data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>),
+                    cudaMemcpyDeviceToHost));
+        file_ptr = static_cast<void *>(static_cast<std::complex<_FloatType> *>(file_ptr) + mrhs_colorspinor_length);  
+    }
+    // 写入dst数据
+    if (lattice_config.m_io_position & IOPosition::IO_FERMION_OUT) {
+        CHECK_CUDA(cudaMemcpy(file_ptr, device_dst_data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>),
+                    cudaMemcpyDeviceToHost));
+        file_ptr = static_cast<void *>(static_cast<std::complex<_FloatType> *>(file_ptr) + mrhs_colorspinor_length);
+    }
+    // 写入gauge数据
+    if (lattice_config.m_io_position & IOPosition::IO_GAUGE) {
+        CHECK_CUDA(cudaMemcpy(file_ptr, device_gauge_data, gauge_length * sizeof(std::complex<_FloatType>),
+                    cudaMemcpyDeviceToHost));
+    }
+}
+
+template <typename _FloatType>
+static void write_to_file_memcpy_CPU (
+        void* file_ptr,                                // 文件映射指针
+        std::complex<_FloatType> *host_src_data,       // 数据指针
+        std::complex<_FloatType> *host_dst_data,       // 数据指针
+        std::complex<_FloatType> *host_gauge_data,     // 数据指针
+        const LatticeConfig& lattice_config
+    )
+{
+    auto mrhs_colorspinor_length = lattice_config.MrhsColorSpinorLength();
+    auto gauge_length = lattice_config.GaugeLength();
+
+    if (lattice_config.m_io_position & IOPosition::IO_FERMION_IN) {
+        memcpy(file_ptr, host_src_data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>));
+        file_ptr = static_cast<void *>(static_cast<std::complex<_FloatType> *>(file_ptr) + mrhs_colorspinor_length);  
+    }
+    // 写入dst数据
+    if (lattice_config.m_io_position & IOPosition::IO_FERMION_OUT) {
+        memcpy(file_ptr, host_dst_data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>));
+        file_ptr = static_cast<void *>(static_cast<std::complex<_FloatType> *>(file_ptr) + mrhs_colorspinor_length);
+    }
+    // 写入gauge数据
+    if (lattice_config.m_io_position & IOPosition::IO_GAUGE) {
+        memcpy(file_ptr, host_gauge_data, gauge_length * sizeof(std::complex<_FloatType>));
+    }
+}
+
+
 template <typename _FloatType>
 void read_from_file (
         std::complex<_FloatType> *device_src_data,       // 数据指针
         std::complex<_FloatType> *device_dst_data,       // 数据指针
         std::complex<_FloatType> *device_gauge_data,     // 数据指针
-        const char *file_path                       // 文件路径
-        // LatticeConfig &lattice_config,              // Lattice规格参数
+        const std::string& file_path,                    // 文件路径
+        LatticeConfig& lattice_config_input
     ) 
 {
     // 打开文件
-    LatticeIOHandler file_handler(file_path);
-    // 读取文件头
-    LatticeConfig lattice_config;
-    FILE *file = fopen(file_path, "rb");
-    if (file == nullptr) {
-
-        throw std::runtime_error(std::string("Failed to open file in function ") + __FUNCTION__ + file_path);
+    LatticeIOHandler file_handler(file_path, LatticeIOHandler::QCU_READ_MODE);
+    struct stat st;
+    if(fstat(file_handler.fd, &st) == -1)  {
+        perror("fstat");
     }
-    else {
-        printf("FILE %s opened correctly\n", file_path);
-    }
+    auto file_size = st.st_size;
 
-    // 读取文件头
-    fread(&lattice_config, sizeof(LatticeConfig), 1, file);
-
-    // 读取src数据
-    fseek(file, 0, SEEK_SET);
-    auto file_size = ftell(file);
-
-    void* data = mmap(nullptr, file_size, PROT_READ, MAP_PRIVATE, file_handler.fd, 0);
-    auto mrhs_colorspinor_length = lattice_config.MrhsColorSpinorLength();
-    auto gauge_length = lattice_config.GaugeLenth();
-
-    // data前移动，跳过文件头
-    data = static_cast<void *>(static_cast<char *>(data) + sizeof(LatticeConfig));
-    if (lattice_config.m_src_rw_flag == ReadWriteFlag::RW_YES) {
-        cudaMemcpy(device_src_data, data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>),
-                    cudaMemcpyHostToDevice);
-        data = static_cast<void *>(static_cast<std::complex<_FloatType> *>(data) + mrhs_colorspinor_length);
-    }
-    // 读取dst数据
-    if (lattice_config.m_dst_rw_flag == ReadWriteFlag::RW_YES) {
-        cudaMemcpy(device_dst_data, data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>),
-                    cudaMemcpyHostToDevice);
-        data = static_cast<void *>(static_cast<std::complex<_FloatType> *>(data) + mrhs_colorspinor_length);
-    }
-    // 读取gauge数据
-    if (lattice_config.m_gauge_rw_flag == ReadWriteFlag::RW_YES) {
-        cudaMemcpy(device_gauge_data, data, gauge_length * sizeof(std::complex<_FloatType>),
-                    cudaMemcpyHostToDevice);
-    }
-    // read over
-}
-
-template <typename _FloatType>
-void write_to_file (
-        std::complex<_FloatType> *device_src_data,       // 数据指针
-        std::complex<_FloatType> *device_dst_data,       // 数据指针
-        std::complex<_FloatType> *device_gauge_data,     // 数据指针
-        const char *file_path,                           // 文件路径
-        LatticeConfig& lattice_config
-    )
-{
-    auto mrhs_colorspinor_length = lattice_config.MrhsColorSpinorLength();
-    auto gauge_length = lattice_config.GaugeLenth();
-
-    complex<_FloatType>* host_src_data = new complex<_FloatType>[mrhs_colorspinor_length];
-    complex<_FloatType>* host_dst_data = new complex<_FloatType>[mrhs_colorspinor_length];
-    complex<_FloatType>* host_gauge    = new complex<_FloatType>[gauge_length];  
-    CHECK_CUDA(cudaMemcpy(host_src_data, device_src_data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>),
-                    cudaMemcpyDeviceToHost)); 
-    CHECK_CUDA(cudaMemcpy(host_dst_data, device_dst_data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>),
-                    cudaMemcpyDeviceToHost)); 
-    CHECK_CUDA(cudaMemcpy(host_gauge, device_gauge_data, gauge_length * sizeof(std::complex<_FloatType>),
-                    cudaMemcpyDeviceToHost));
-
-    cout << "DEBUG, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>) = " << 
-                    mrhs_colorspinor_length * sizeof(std::complex<_FloatType>) 
-                    << " " << host_gauge[0] << endl;
-
-
-    // 打开文件
-    LatticeIOHandler file_handler(file_path);
-    // 读取文件头
-    // LatticeConfig lattice_config;
-    FILE *file = fopen(file_path, "wb");
-    if (file == nullptr) {
-        throw std::runtime_error(std::string("Failed to open file in function ") + __FUNCTION__ + file_path);
-    }
-    else {
-        printf("FILE %s opened correctly\n", file_path);
-    }
-    // 写文件头
-    fwrite(&lattice_config, sizeof(LatticeConfig), 1, file);
-
-    fclose(file);
-
-    //获取文件属性
-    struct stat sb;
-    if(fstat(file_handler.fd, &sb) == -1)  perror("fstat");
-    auto appendSize = sizeof(LatticeConfig) + 
-            (2 * mrhs_colorspinor_length + gauge_length) * sizeof (complex<_FloatType>);
-
-    ftruncate(file_handler.fd, appendSize);
-    fsync(file_handler.fd);
-
-    printf("======fd = %d, sb.st_size = %ld, appendSize = %ld, sb.st_size + appendSize = %ld\n", 
-                    file_handler.fd, sb.st_size, appendSize, sb.st_size + appendSize);
-    
-    void* origin_data = mmap(nullptr, sb.st_size + appendSize, PROT_WRITE | PROT_READ, 
-                            MAP_SHARED, file_handler.fd, 0);
+    // mmap 映射
+    void* origin_data = mmap(nullptr, file_size, PROT_READ, MAP_PRIVATE, file_handler.fd, 0);
     void* data = origin_data;
     if (data == MAP_FAILED || data == nullptr) {
         throw std::runtime_error("MMAP failed\n");
@@ -132,53 +100,99 @@ void write_to_file (
         printf("MMAP correct\n");
     }
 
+    // 读取文件头
+    LatticeConfig lattice_config_file;
+    memcpy(&lattice_config_file, data, sizeof(LatticeConfig));
+    auto mrhs_colorspinor_length = lattice_config_file.MrhsColorSpinorLength();
+    auto gauge_length = lattice_config_file.GaugeLength();
+    // 检查mpi参数、lattice参数和Nc，mIn参数是否一致
+    if (!lattice_config_input.check_config(lattice_config_file)) {
+        throw std::runtime_error("input config is not the same as file config\n");
+    } else {
+        fprintf(stdout, "terminal paramters matched\n");
+    }
+    // 接下来拷贝文件头的剩余不检查部分
+    lattice_config_input.copy_info(lattice_config_file);
+
     // data前移动，跳过文件头
     data = static_cast<void *>(static_cast<char *>(data) + sizeof(LatticeConfig));
-
-    // cout << "DEBUG, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>) = " << 
-    //                 mrhs_colorspinor_length * sizeof(std::complex<_FloatType>) 
-    //                 << " " << host_dst_data[0] << endl;
-
-    if (lattice_config.m_src_rw_flag == ReadWriteFlag::RW_YES) {
-        memcpy((char*)data, host_src_data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>));
-        // CHECK_CUDA(cudaMemcpy(data, device_src_data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>),
-        //             cudaMemcpyDeviceToHost));
-        data = static_cast<void *>(static_cast<std::complex<_FloatType> *>(data) + mrhs_colorspinor_length);  
+    if (lattice_config_file.m_io_position & IOPosition::IO_FERMION_IN) {
+        CHECK_CUDA(cudaMemcpy(device_src_data, data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>),
+                    cudaMemcpyHostToDevice));
+        data = static_cast<void *>(static_cast<std::complex<_FloatType> *>(data) + mrhs_colorspinor_length);
     }
     // 读取dst数据
-    if (lattice_config.m_dst_rw_flag == ReadWriteFlag::RW_YES) {
-
-        memcpy((char*)data, host_dst_data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>));
-        // CHECK_CUDA(cudaMemcpy(data, device_dst_data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>),
-        //             cudaMemcpyDeviceToHost));
+    if (lattice_config_file.m_io_position & IOPosition::IO_FERMION_OUT) {
+        CHECK_CUDA(cudaMemcpy(device_dst_data, data, mrhs_colorspinor_length * sizeof(std::complex<_FloatType>),
+                    cudaMemcpyHostToDevice));
         data = static_cast<void *>(static_cast<std::complex<_FloatType> *>(data) + mrhs_colorspinor_length);
     }
     // 读取gauge数据
-    if (lattice_config.m_gauge_rw_flag == ReadWriteFlag::RW_YES) {
-        memcpy((char*)data, host_gauge, gauge_length * sizeof(std::complex<_FloatType>));
-        // CHECK_CUDA(cudaMemcpy(data, device_gauge_data, gauge_length * sizeof(std::complex<_FloatType>),
-        //             cudaMemcpyDeviceToHost));
+    if (lattice_config_file.m_io_position & IOPosition::IO_GAUGE) {
+        CHECK_CUDA(cudaMemcpy(device_gauge_data, data, gauge_length * sizeof(std::complex<_FloatType>),
+                    cudaMemcpyHostToDevice));
     }
-    if((msync((void*)origin_data, sb.st_size + appendSize, MS_SYNC)) == -1)  perror("msync");
-    if((munmap((void *)origin_data, sb.st_size + appendSize)) == -1)  perror("munmap\n");
-
-    // write over
-    delete[] host_dst_data;
-    delete[] host_src_data;
-    delete[] host_gauge;
+    // read over
+    if((msync((void*)origin_data,   file_size, MS_SYNC)) == -1)  { perror("msync");   }
+    if((munmap((void *)origin_data, file_size         )) == -1)  { perror("munmap\n");}
 }
 
+
+template <typename _FloatType>
+void write_to_file (
+        std::complex<_FloatType> *device_src_data,       // 数据指针
+        std::complex<_FloatType> *device_dst_data,       // 数据指针
+        std::complex<_FloatType> *device_gauge_data,     // 数据指针
+        const std::string& file_path,                    // 文件路径
+        LatticeConfig& lattice_config
+    )
+{
+    auto mrhs_colorspinor_length = lattice_config.MrhsColorSpinorLength();
+    auto gauge_length = lattice_config.GaugeLength();
+
+    // 打开文件
+    LatticeIOHandler file_handler(file_path, LatticeIOHandler::QCU_READ_WRITE_MODE);
+
+    auto newFileSize = sizeof(LatticeConfig) + 
+            (2 * mrhs_colorspinor_length + gauge_length) * sizeof (complex<_FloatType>);
+
+    ftruncate(file_handler.fd, newFileSize);
+    fsync(file_handler.fd);
+
+    printf("===== DEBUG: fd = %d, newfilesize = %ld\n", file_handler.fd, newFileSize);
+    void* origin_data = mmap(nullptr, newFileSize, PROT_WRITE | PROT_READ, MAP_SHARED, file_handler.fd, 0);
+    void* data = origin_data;
+    if (data == MAP_FAILED || data == nullptr) {
+        throw std::runtime_error("MMAP failed\n");
+    } 
+    else {
+        printf("MMAP correct\n");
+    }
+
+    // 写文件头
+    memcpy(data, &lattice_config, sizeof(LatticeConfig));
+    // data前移动，跳过文件头
+    data = static_cast<void *>(static_cast<char *>(data) + sizeof(LatticeConfig));
+
+    write_to_file_memcpy_GPU<_FloatType>(data, device_src_data, device_dst_data, device_gauge_data, lattice_config);
+
+    if((msync((void*)origin_data, newFileSize, MS_SYNC)) == -1)  perror("msync");
+    if((munmap((void *)origin_data, newFileSize)) == -1)  perror("munmap\n");
+}
+
+// 目前只实例化了双精度实例
 template void read_from_file(
         std::complex<double> *device_src_data,       // 数据指针
         std::complex<double> *device_dst_data,       // 数据指针
         std::complex<double> *device_gauge_data,     // 数据指针
-        const char *file_path                       // 文件路径
+        const std::string& file_path,                // 文件路径
+        LatticeConfig& lattice_config_input          // 命令行文件头，和文件存储文件头进行对比
     );  
 
 template void write_to_file(
         std::complex<double> *device_src_data,       // 数据指针
         std::complex<double> *device_dst_data,       // 数据指针
         std::complex<double> *device_gauge_data,     // 数据指针
-        const char *file_path,                       // 文件路径
+        const std::string& file_path,                // 文件路径
         LatticeConfig& lattice_config
-    );  
+    );
