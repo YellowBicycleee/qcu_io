f956690b1d025b5c83b6a9802d85cfd181a63583
YellowBicycleee <wangjianchengcs@163.com>
2024-09-19 01:32:34 +0800

    add: 增加qcu_public文件，使得枚举独立到一个文件中

---
+++ include/qcu_public.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cstdint>
+constexpr int MAGIC_BYTE_SIZE = 4;
+
+// 1 Byte
+enum class DataFormat : uint8_t {
+    FORMAT_UNKNOWN = 0,
+    QUDA_FORMAT,
+    QUDA_FORMAT_EO_PRECONDITONED,
+    QDP_FORMAT,
+};
+enum class StoragePrecision : uint8_t {
+    PRECISION_UNKNOWN = 0,
+    PRECISION_HALF,
+    PRECISION_FLOAT,
+    PRECISION_DOUBLE
+};
+
+enum class StorageType : uint8_t {
+    TYPE_UNKNOWN = 0,
+    TYPE_FERMION,
+    TYPE_GAUGE
+};
+
+enum class MrhsShuffled : uint8_t {
+    MRHS_SHUFFLED_NO = 0,   // 多个mrhs并揉在一起
+    MRHS_SHUFFLED_YES,      // m个向量的元素相邻
+};
+
+enum LatticeDimension : int32_t {
+    X_DIM = 0,
+    Y_DIM,
+    Z_DIM,
+    T_DIM,
+    Nd
+};
