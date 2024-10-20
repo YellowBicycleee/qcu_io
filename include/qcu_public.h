1fda46e8c2095d26d3c446b4f8f468457b7df6c6
YellowBicycleee <wangjianchengcs@163.com>
2024-10-20 21:25:34 +0800

    modify: 删除部分无用代码

--- include/qcu_public.h
+++ include/qcu_public.h
@@ -4,11 +4,6 @@
 
 #define NOT_IMPLEMENTED "Not implemented yet\n" 
 
-#define QCU_DEVICE __device__ 
-#define QCU_HOST __host__
-#define QCU_DEVICE_HOST __host__ __device__
-#define QCU_GLOBAL __global__
-
 constexpr int MAGIC_BYTE_SIZE = 4;
 constexpr int Nd = 4; // 4维
 constexpr int Ns = 4; // nSpinor = 4
@@ -20,12 +15,6 @@ enum class DataFormat : uint8_t {
     QUDA_FORMAT_EO_PRECONDITONED,
     QDP_FORMAT,
 };
-// enum class QCU_PRECISION : uint8_t {
-//     PRECISION_UNKNOWN = 0,
-//     PRECISION_HALF,
-//     PRECISION_FLOAT,
-//     PRECISION_DOUBLE
-// };
 
 enum class StorageType : uint8_t {
     TYPE_UNKNOWN = 0,
@@ -38,12 +27,6 @@ enum class MrhsShuffled : uint8_t {
     MRHS_SHUFFLED_YES,      // m个向量的元素相邻
 };
 
-// enum LatticeDimension : int32_t {
-//     X_DIM = 0,
-//     Y_DIM,
-//     Z_DIM,
-//     T_DIM,
-// };
 
 // FROM QCU
 // clang-format off
